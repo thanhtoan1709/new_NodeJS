986f496398a185a5981419a039f9512ff4e71054
Toan Doan <thanhmyloipro@gmail.com>
2023-11-05 13:01:57 +0700

    create courses

--- src/app/controller/CoursesController.js
+++ src/app/controller/CoursesController.js
@@ -17,7 +17,12 @@ class CoursesController {
     // POST courses/store
     
     store(req,res,next) {
-      res.json(req.body)
+    const formData = req.body;
+    const course = new Course(formData);
+    course.save()
+        .then(() => res.redirect('/'))
+        .catch(error => next(error));
+        
     }
 }
 module.exports =  new CoursesController;
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
+var slug = require('mongoose-slug-updater');
+
+mongoose.plugin(slug)
 const Schema = mongoose.Schema;
 
-
 const Course = new Schema({
-    name: {type :String},
+    name: {type :String, required: true,},
     description: {type :String},
-    image: {type :String},
-    createdAt: {
-        type : Date,default: Date.now
-    } ,
-    updatedAt: {
-        type : Date,default: Date.now
-    } ,
-    
+    image: {type :String,},
+    videoID: {type :String, required: true},
+    level: {type :String},  
+    slug : {type : String,slug : 'name',unique:true},
+},{
+    timestamps:true
 });
 module.exports = mongoose.model('Course',Course);
